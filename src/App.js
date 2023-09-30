f3c423a5e375c3b182f9ef300776862abc09be59
oluwagbogo <oluwagbogoadebanjo@gmail.com>
2023-09-30 20:30:46 +0100

    Added darkmode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import { useState } from 'react';
 import './App.css';
 import TodoList from './components/TodoList';
+import { createContext } from 'react'
+import ReactSwitch from 'react-switch';
 
+export const ThemeContext = createContext(null);
 function App() {
+  const [theme, setTheme] = useState("dark");
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
+
   return (
-    <div className="todo-app">
-      <TodoList />
-    </div>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className={`todo-app ${theme}-mode`}>
+        <label> {theme === "light" ? "Light Mode" : "Dark Mode"}</label>
+        <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} />
+        <TodoList />
+      </div>
+      </ThemeContext.Provider>
+
   );
 }
 
